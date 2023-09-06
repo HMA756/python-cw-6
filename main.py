4af4482e5b3ff12111f90d3605c89cf1bfd57da3
HMA756 <hooralmulla22@gmail.com>
2023-09-06 19:46:46 +0300

    CW6

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# write your code here
+person = {
+    'name':'Hoor',
+    'age' : 14,
+    'hobbies':['gaming','sketching','photoshopping']
+}
+print(person['name'])
+print(person['age'])
+
+person['age']=15
+person['country']= 'Australia'
+print(person)
+print(len(person))
+
+person['hobbies'].append('reading')
+print(person)
+def check_hobbies(person):
+    if len(person['hobbies'])>=3:
+        print('WOW YOU ARE AMAZING')
+    else :
+        print('error')
+
+check_hobbies(person)
+
